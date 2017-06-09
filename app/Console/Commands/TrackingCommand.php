d89ed676f9a1f16c666dfa7dab2449fbc346a65d
Aleksey Dorokhov <akeinhell@gmail.com>
2017-06-09 12:54:35 +0700

    fix bug

--- app/Console/Commands/TrackingCommand.php
+++ app/Console/Commands/TrackingCommand.php
@@ -7,7 +7,6 @@ use App\Games\Interfaces\CanTrackingInterface;
 use App\Telegram\Bot;
 use App\Telegram\Config;
 use Illuminate\Console\Command;
-use Illuminate\Support\Collection;
 
 class TrackingCommand extends Command
 {
@@ -45,38 +44,27 @@ class TrackingCommand extends Command
     public function handle()
     {
         $chats = \Track::getChatList();
-	dump($chats);
 //        $chats = [94986676];
+        dump($chats);
         foreach ($chats as $chatId) {
             try {
                 $cacheKey = 'TRACK:' . $chatId;
                 $this->info('scan ' . $cacheKey);
-                $engine = $this->getEngine($chatId);
+                $engine          = $this->getEngine($chatId);
                 $actualLevelList = $engine->getQuestList();
-//                 $actualLevelList = collect([
-//                    1 => 'test 1',
-//                    2 => 'test2',
-//                    3 => 'test3',
-//                    4 => 'test4',
-//                    5 => 'test5',
-//                ]);
-//		dump('actual', $actualLevelList);
-                $oldLevels       = \Cache::get($cacheKey, []);
-//		dump('old', $oldLevels);
 
-                $diffKeys = array_flip(array_diff($actualLevelList, $oldLevels));
-//                dump('diff ', $diffKeys);
+                $oldLevels = \Cache::get($cacheKey, []);
+
+                $diffKeys = array_diff($actualLevelList, $oldLevels);
                 if ($diffKeys) {
-                    $newSet = array_flip(array_merge($oldLevels, $diffKeys));
-//		    dump('newSet', $newSet);
+                    $newSet = $oldLevels + $diffKeys;
                     \Cache::put($cacheKey, $newSet, 60 * 24 * 7);
                     Bot::action()->sendMessage($chatId, $this->formatMessage($diffKeys));
-                    
+
                     if ($engine instanceof CanTrackingInterface) {
-//                   	dump('fetch', $newSet);                
-                  	foreach($newSet as $id => $title) {
-			    $this->info('fetch:    '.$id. ':  '.$title);
-			} 
+                        foreach ($newSet as $id => $title) {
+                            $this->info('fetch:    ' . $id . ':  ' . $title);
+                        }
                     }
                 }
             } catch (\Exception $e) {
