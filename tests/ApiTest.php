879d692c0a8b9873b27959dbf99335639b788803
Aleksey Dorokhov <akeinhell@gmail.com>
2017-07-21 17:03:37 +0700

    disable test coordss

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -6,7 +6,7 @@ class ApiTest extends TestCase
     public function testBasicExample()
     {
         $this
-            ->get('api/en/games/demo.en.cx')
+            ->get('api/en/games/msk.en.cx')
             ->seeJsonStructure([
                 '*' => ['id', 'type', 'domain', 'start', 'title'],
             ]);
