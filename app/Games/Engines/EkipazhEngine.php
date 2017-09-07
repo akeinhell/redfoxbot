86d7dcdbff00cb5b0223b86f1fba019ce3b2a62f
Aleksey Dorokhov <akeinhell@gmail.com>
2017-09-07 18:24:43 +0700

    fix ekipazh

---
+++ app/Games/Engines/EkipazhEngine.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akeinhell
+ * Date: 29.05.2016
+ * Time: 0:33.
+ */
+
+namespace App\Games\Engines;
+
+use App\Exceptions\TelegramCommandException;
+use App\Games\BaseEngine\AbstractGameEngine;
+use App\Games\Sender;
+use App\Telegram\Config;
+use Illuminate\Support\Collection;
+
+class EkipazhEngine extends DozorLiteEngine
+{
+
+}
