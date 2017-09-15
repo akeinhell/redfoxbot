41e0779879d1799ee18026107a76c24367f1d9f1
Aleksey Dorokhov <akeinhell@gmail.com>
2017-09-15 18:44:55 +0700

    add metric to sendcode

--- app/Telegram/Handlers/CodeHandler.php
+++ app/Telegram/Handlers/CodeHandler.php
@@ -10,15 +10,17 @@ use TelegramBot\Api\Types\Update;
 
 class CodeHandler extends BaseHandler
 {
-    public function run(Update $update) {
-        \Log::debug('Run CodeHandler');
+    public function run(Update $update)
+    {
+        $time    = microtime(true);
         $message = $update->getMessage();
-        $chatId = $message->getChat()->getId();
+        $chatId  = $message->getChat()->getId();
         $command = new CodeCommand($chatId);
         $command->execute(ltrim($message->getText(), '!'));
         if ($command->getResponseText()) {
             $reply = $command->getResponseReply() ? $message->getMessageId() : null;
             Bot::sendMessage($chatId, $command->getResponseText(), $command->getResponseKeyboard(), $reply);
         }
+        \Log::debug('Run CodeHandler: ' . microtime(true) - $time);
     }
 }
