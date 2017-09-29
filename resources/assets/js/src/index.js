fa78f1de432b10d4a9a64c933450d7e3313ec0a7
Aleksey Dorokhov <akeinhell@gmail.com>
2017-09-29 10:59:19 +0700

    dev

---
+++ resources/assets/js/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import 'grommet/scss/vanilla/index.scss';
+
+const element = document.getElementById('root');
+ReactDOM.render(<div>test</div>, element);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,59 @@
+let path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin      = require('uglifyjs-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const uglifyPlugin = new UglifyJSPlugin({
+    // beautify: true,
+    // comments: true,
+    compress: {
+        // sequences: isProduction,
+        // booleans: isProduction,
+        // loops: isProduction,
+        unused: true,
+        warnings: true,
+        drop_console: true,
+        //     unsafe: false
+    },
+    warnings: true,
+    mangle: false,
+});
 
 const mainConfig = {
     name: 'main',
     entry: {
-        index: './',
+        index: './resources/assets/js/src/index.js',
         styles: './resources/assets/sass/main.scss',
+        vendor: [
+            'react',
+            'react-dom',
+            'grommet',
+        ]
     },
     output: {
         path: __dirname + '/public/dist/',
-        filename: "js/[name].js",
-        sourceMapFilename: "js/[name].map",
+        filename: 'js/[name]_[chunkhash:4].js',
+        sourceMapFilename: 'js/[name].[chunkhash:4].map',
         publicPath: '/dist/'
     },
     module: {
         loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            'es2015',
+                            'stage-2',
+                            'react',
+                        ],
+                        plugins: ["transform-es2015-destructuring", "transform-object-rest-spread"],
+                    }
+                }]
+            },
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
@@ -26,7 +65,17 @@ const mainConfig = {
                 test: /\.(jpe?g|png|gif)$/i,
                 use: 'file-loader?name=images/[name].[ext]'
             },
-
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=/fonts/[name].[ext]'
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader!less-loader'
+                })
+            },
         ]
     },
     resolve: {
@@ -34,12 +83,46 @@ const mainConfig = {
     },
     devtool: '#cheap-module-source-map',
     plugins: [
+        function () {
+            this.plugin('done', function (stats) {
+                let data = stats.toJson().assetsByChunkName;
+
+                let out = Object.keys(data).reduce((prev, current) => {
+                    let files = Array.isArray(data[current]) ? data[current] : [data[current]];
+                    prev[current] = files.reduce((res, current) => {
+                        let ext = path.extname(current).slice(1);
+                        res[ext] = '/' + ext + '/' + current.replace(`/${ext}`, '').replace('../', '');
+                        return res;
+                    }, {});
+
+
+                    return prev;
+                }, {});
+                require('fs').writeFileSync(
+                  path.join(__dirname, 'stats.json'),
+                  JSON.stringify(out));
+            });
+        },
+        new CleanWebpackPlugin(['public/dist/*'], {
+            root: __dirname,
+            verbose: true,
+            // dry: false,
+        }),
+        // extractVersions.bind(this),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: [
+                'vendor',
+            ],
+            async: true,
+            minChunks: Infinity
+        }),
         new webpack.DefinePlugin({DEBUG: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))}),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
             minChunks: Infinity
         }),
-        new ExtractTextPlugin('css/style.css')
+        new ExtractTextPlugin('css/style.css'),
+        uglifyPlugin
     ]
 };
 
